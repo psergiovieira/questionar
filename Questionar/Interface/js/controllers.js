d3f9788f5176f6641f48ec1365e1a172996cb822
Paulo Sérgio <Paulo Srgio>
2016-04-19 01:41:23 -0300

    integrando interface com webservice

--- Questionar/Interface/js/controllers.js
+++ Questionar/Interface/js/controllers.js
@@ -1,4 +1,5 @@
 var publicModules = angular.module('public', []);
+var urlApi = 'http://localhost/Questionar.WebApi/api/';
 
 publicModules.controller('Home', ['$scope', '$http',
   function ($scope, $http) {
@@ -6,14 +7,19 @@ publicModules.controller('Home', ['$scope', '$http',
    $scope.Teste = 'teste binding...';
   }]);
 
-publicModules.controller('Account', ['$scope', '$http',
+publicModules.controller('Account', ['$scope', '$http',  
   function ($scope, $http) {
 
    $scope.user = {};
    $scope.save = function() {
 
    	var data = $scope.user;
-   };
-
+   	return $http({
+                url: urlApi + 'User/Post',
+                method: 'POST',
+                data: angular.toJson(data)
+            });
+  
+	}
   }]);
 
