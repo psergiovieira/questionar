88be10369548e136bbd5438869b73d9e7cd99d4a
Paulo Sérgio <psvieira.ti@gmail.com>
2016-05-27 21:42:07 -0300

    Exceção criada para exceção de regra de negócio

---
+++ Questionar/Domain/Exceptions/QuestionarException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Domain.Exceptions
+{
+    public class QuestionarException : Exception
+    {
+        public QuestionarException(string message)
+           : base(message)
+        {
+            
+        }
+    }
+}
