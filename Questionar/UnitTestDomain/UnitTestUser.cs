9cc2d0df101f49f958414bdd8002f0b26e9081d3
Paulo Sérgio <psvieira.ti@gmail.com>
2016-05-28 00:20:52 -0300

    adicionando moq no projeto e realizando o primeiro teste

--- Questionar/UnitTestDomain/UnitTestUser.cs
+++ Questionar/UnitTestDomain/UnitTestUser.cs
@@ -7,19 +7,46 @@ using Data;
 using Data.Security;
 using NHibernate.Linq;
 using System.Linq;
+using Infraestructure;
+using Moq;
 
 namespace UnitTestDomain
 {
     [TestClass]
     public class UnitTestUser
     {
-        [TestMethod]
-        public void GetUser()
+        private Mock<IRepository<User>> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private UserManager _manager;
+        private User _user;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            var _unitOfWork = new NhibernateUnitOfWork();
-            var _repository = new NHibernateRepository<User>(_unitOfWork);
-            var _manager = new UserManager(_repository, _unitOfWork);
-            var user = _manager.Repository.GetById(1);            
+            _mockRepository = new Mock<IRepository<User>>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _manager = new UserManager(_mockRepository.Object,_mockUnitOfWork.Object);
+            _user = new User
+            {
+                Id = 1,
+                Name = "Paulo",
+                UserName = "psvieira",
+                Password = "123456",
+                Created = new DateTime(2015,1,10),
+                Active = true,
+                IsTeacher = false,
+                Email = "psvieira.ti@gmail.com"
+            };
+        }
+
+        [TestMethod]
+        public void CanGetUser()
+        {
+            _mockRepository.Setup(x => x.GetById(1)).Returns(_user);
+
+            var user = _manager.Repository.GetById(1);
+
+            Assert.AreEqual(user, _user);
         }
     }
 }
