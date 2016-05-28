38a0bb6c96516b034c803d885a86bbcf4f3b1d51
Paulo Sérgio <psvieira.ti@gmail.com>
2016-05-27 21:41:20 -0300

    validações no cadastro de usuário

--- Questionar/Domain/Manager/UserManager.cs
+++ Questionar/Domain/Manager/UserManager.cs
@@ -1,5 +1,6 @@
 ﻿using Data;
 using Data.Security;
+using Domain.Exceptions;
 using Domain.Helper;
 using Infraestructure;
 using Infraestructure.Business;
@@ -21,6 +22,16 @@ namespace Domain.Manager
 
         public void Create(User user)
         {
+            var emptyField = string.IsNullOrEmpty(user.Email) || string.IsNullOrEmpty(user.UserName) || string.IsNullOrEmpty(user.Name) || string.IsNullOrEmpty(user.Password);
+            if (emptyField)
+                throw new QuestionarException("Preencha os campos obrigatórios!");
+
+            if (Repository.Query().Any(c => c.UserName.ToLower() == user.UserName.ToLower()))
+                throw new QuestionarException("Nome de usuário já registrado, por favor informe um nome de usuário válido!");
+
+            if (Repository.Query().Any(c => c.Email.ToLower() == user.Email.ToLower()))
+                throw new QuestionarException("E-mail já cadastrado, por favor informe um email válido!");      
+
             Transaction(()=>
             {
                 user.Password = Password.Encrypty(user.Password);
