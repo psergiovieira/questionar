64ea1bb718b7458f7c14cf682d82c3ed42716690
Paulo Sérgio <psvieira.ti@gmail.com>
2016-06-15 15:08:37 -0300

    frontend da busca por disciplinas

--- Questionar/Interface/app/admin/student/home.controller.js
+++ Questionar/Interface/app/admin/student/home.controller.js
@@ -1,6 +1,15 @@
 privateModules.controller('HomeStudent', ['$scope', '$http', 'dialog' ,
   function ($scope, $http, dialog) {
  
-   
+   $scope.inputSearch = '';
+   $scope.courses = {};
+   $scope.search = function() {
+    	return $http({url: urlApi + 'Course/Get?search=' + $scope.inputSearch,method: 'GET'})
+       .success(function(result) {
+           $scope.courses = result;
+        }).error(function(result) {
+            $scope.courses = [];    
+        }); 
+	}
 	
   }]);
