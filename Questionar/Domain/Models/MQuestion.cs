c843cfd75d00112d44f40e21d40841f1793cc9f3
Paulo Sérgio <paulo.lemaf@gmail.co>
2016-07-01 15:19:05 -0300

    passando model para domain, já que será utilizada para retornar o agrupamento de questões e alternativas

---
+++ Questionar/Domain/Models/MQuestion.cs
@@ -0,0 +1,14 @@
+﻿using Data;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class MQuestion
+    {
+        public Course Course { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Alternative> Alternatives { get; set; }
+    }
+}
