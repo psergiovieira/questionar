17cf5791bc88f2caaa37279a42d85f57fcf3e1ae
Paulo Sérgio <psvieira.ti@gmail.com>
2016-07-19 22:41:50 -0300

    inicio da logica para responnder questões

---
+++ Questionar/ApiQuestionar/Controllers/AnswerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ApiQuestionar.Auxiliary;
+using Data;
+using Domain.Manager;
+using Infraestructure.Repository;
+
+namespace ApiQuestionar.Controllers
+{
+    public class AnswerController : BaseController<Answer>
+    {
+        private AnswerManager _manager;
+
+        public AnswerController()
+        {
+            _manager = new AnswerManager(Repository, UnitOfWork);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult Post(Alternative alternative)
+        {
+            var alternativeManager = new AlternativeManager(new NHibernateRepository<Alternative>(UnitOfWork), UnitOfWork);
+
+            _manager.Reply(alternative.Id,this.GetUser(),alternativeManager);
+            return Ok("Questão cadastrada com sucesso!");
+        }
+    }
+}
---
+++ Questionar/Domain/Manager/AnswerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Security;
+using Domain.Exceptions;
+using Infraestructure;
+using Infraestructure.Business;
+using Infraestructure.UnitOfWork;
+
+namespace Domain.Manager
+{
+    public class AnswerManager : Business<Answer>
+    {
+        public AnswerManager(IRepository<Answer> repository, IUnitOfWork unitOfWork)
+            : base(repository, unitOfWork)
+        {
+        }
+
+        public bool Reply(int idAlternative, User student, AlternativeManager alternativeManager)
+        {
+            var alternative = alternativeManager.Repository.GetById(idAlternative);
+                if (alternative == null)
+                    throw new QuestionarException("Alternativa inválida");
+
+            Transaction(() =>
+            {
+                var answer = new Answer()
+                {
+                    Alternative = alternative,
+                    Student = student,
+                    Created = DateTime.Now
+                };
+
+                Repository.Create(answer);
+            });
+
+            return alternative.IsCorrect;
+        }
+    }
+}
